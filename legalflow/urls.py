5d264a59ef6cd9cbce97b0b1e28eb5e05f83018a
F3rreir4L19 <oleviferreira118@gmail.com>
2025-12-11 09:11:23 -0300

    Add files via upload

---
+++ legalflow/urls.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from core.admin_site import admin_site
+urlpatterns = [
+    # Admin
+    path('admin/', admin_site.urls),
+    
+    # API Authentication
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # API Endpoints
+    path('api/', include('core.urls')),
+]
+
+# Servir arquivos de mídia em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Customização do Admin
+admin.site.site_header = "LegalFlow"
+admin.site.site_title = "LegalFlow Admin"
+admin.site.index_title = "Painel de Controle"
